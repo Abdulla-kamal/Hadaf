93b61d126dd7e58eeef1a884f3d9533f97990c99
Abdulla-Kamal <elhassiabdo@gmail.com>
2023-11-20 09:42:41 +0200

    Store Compltion

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,43 @@
-fetch("./data.json")
+fetch("./products.json")
 .then(res=>res.json())
-.then(products=> {
-   console.log(products)
+.then(objProduct=> {
+   let arrOgProducts = Object.keys(objProduct);
+
+   // select container of boxes 
+   let container = document.querySelector(".articles .container");
+   console.log()
+   arrOgProducts.forEach(product=> {
+      let box = document.createElement("div");
+      // container ... box 
+      box.className = "box";
+      let img = document.createElement('img');
+      img.setAttribute("src", objProduct[product][0]);
+      console.log(img)
+      let content = document.createElement("div");
+      // container ... box ...  content 
+      content.className = "content";
+      let h3 = document.createElement("h3");
+      h3.innerHTML = `${product} - ${objProduct[product][1]}`;
+      let p = document.createElement("p");
+      p.innerHTML = objProduct[product][2];
+      content.appendChild(h3);
+      content.appendChild(p);
+      // container ... box ... info
+      let info = document.createElement("div");
+      info.className = "info";
+      let a = document.createElement("a");
+      a.setAttribute("href", "https://api.whatsapp.com/send/?phone=218924245371&text&type=phone_number&app_absent=0");
+      a.innerHTML = "Contact Us"
+      let i = document.createElement("i");
+      i.className = "fas fa-long-arrow-alt-right";
+      info.appendChild(a);
+      info.appendChild(i);
+      //
+      box.appendChild(img);
+      box.appendChild(content);
+      box.appendChild(info);
+      container.appendChild(box)
+   })
 })
+
+ 
